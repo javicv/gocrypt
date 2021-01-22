74743f234dc2f94ad6246feede0efdaa1e9086e4
Javier Collado <javicv@gmail.com>
2021-01-22 21:21:18 +0100

    Command line parameters are processed using 'flags'

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -10,35 +11,62 @@ const _keySize int = 16
 const _saltSize int = 16
 const _iterations int = 32767
 
+var method string
+var password string
+var value string
+
+func init() {
+	const (
+		methodDefault   = "encrypt"
+		methodUsage     = "Method to use encrypt/decrypt"
+		passwordDefault = ""
+		passwordUsage   = "Password to encrypt/decrypt the value"
+		valueDefault    = ""
+		valueUsage      = "Value to encrypt/decrypt"
+	)
+	flag.StringVar(&method, "m", methodDefault, methodUsage)
+	flag.StringVar(&method, "method", methodDefault, methodUsage)
+	flag.StringVar(&password, "p", passwordDefault, passwordUsage)
+	flag.StringVar(&password, "password", passwordDefault, passwordUsage)
+	flag.StringVar(&value, "v", valueDefault, valueUsage)
+	flag.StringVar(&value, "value", valueDefault, valueUsage)
+}
+
 func main() {
-	if len(os.Args) < 4 {
-		fmt.Fprintf(os.Stderr, `To encrypt/decrypt values
-use: %s [-e|-d] PASSWORD VALUE
-	-e: Encrypt value
-	-d: Decrypt value`, os.Args[0])
+	flag.Parse()
+
+	if method != "encrypt" && method != "decrypt" {
+		fmt.Fprintf(os.Stderr, "Wrong Method '%s'. Only 'encrypt' or 'decrypt' are allowed.\n", method)
 		os.Exit(1)
 	}
-	if os.Args[1] != "-e" && os.Args[1] != "-d" {
-		fmt.Fprintf(os.Stderr, "Wrong Method %s. Only -e or -d are allowed.\n", os.Args[1])
+	if len(password) == 0 {
+		fmt.Fprintf(os.Stderr, "You must provide the encryption password.\n")
+		flag.PrintDefaults()
+		os.Exit(2)
 	}
-	method := os.Args[1]
-	password := os.Args[2]
-	value := os.Args[3]
+	if len(value) == 0 {
+		fmt.Fprintf(os.Stderr, "One value must be provided to be encrypted/decrypted.\n")
+		flag.PrintDefaults()
+		os.Exit(3)
+	}
 
 	var result string
 	var err error
-	if method == "-e" {
+	switch method {
+	case "encrypt":
 		result, err = encryptString(value, password)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-			os.Exit(2)
-		}
-	} else {
-		result, err = decryptString(value, password)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 			os.Exit(4)
 		}
+		break
+	case "decrypt":
+		result, err = decryptString(value, password)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+			os.Exit(5)
+		}
+		break
 	}
 	fmt.Printf("%s: %s\n", value, result)
 }
